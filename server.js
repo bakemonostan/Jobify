7032aa11d33251da0fce565c81b624e420d55ce9
bakemonostan <ehizomokhomion@gmail.com>
2022-11-01 05:31:39 -0700

    set up backend server and 404 route

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+
+//middleware
+import notFoundMiddleware from './middleware/not-found.js';
+
+app.get('/', (req, res) => {
+  res.send('Welcome');
+});
+
+app.use(notFoundMiddleware);
+
+// declare port variable
+const port = process.env.PORT || 5000;
+// setting up the port
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
