019737af0b1ad65493883891410a9f92193318cf
bakemonostan <ehizomokhomion@gmail.com>
2022-11-01 06:04:53 -0700

    connect to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+
+import connectDB from './db/connect.js';
 
 //middleware
 import notFoundMiddleware from './middleware/not-found.js';
@@ -16,6 +20,17 @@ app.use(notFoundMiddleware);
 // declare port variable
 const port = process.env.PORT || 5000;
 // setting up the port
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+
+// this would only run if the connection to the database is succesful, thats why we have the app listening inside the start() function
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
