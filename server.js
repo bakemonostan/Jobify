4b3f6c1360c316757a95a690c5c665870ddcda46
bakemonostan <ehizomokhomion@gmail.com>
2022-11-05 14:19:13 -0700

    create register route

--- server.js
+++ server.js
@@ -23,8 +23,16 @@ app.get('/', (req, res) => {
   res.send('Welcome');
 });
 
-app.use('api/v1/auth', authRouter);
-app.use('api/v1/jobs', jobsRouter);
+app.get('/api/v1/auth', (req, res) => {
+  res.send('Welcome Again');
+});
+
+app.get('/api/v1/auth/register', (req, res) => {
+  res.send('Welcome to get');
+});
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
 
 app.use(errorHandlerMiddleware);
 app.use(notFoundMiddleware);
